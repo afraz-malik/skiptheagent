309436c2993df5071c7369c1a3c265c5ef6eb367
Afraz Malik <afrazmalik321@gmail.com>
2021-06-22 22:56:37 +0500

    'updating'

--- client/src/components/Ownershipform/Ownershipform.jsx
+++ client/src/components/Ownershipform/Ownershipform.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import OwnershipformCss from './Ownershipform.module.scss'
-import { Link } from 'react-router-dom'
-
-import BoxModel from '../boxModel/boxModel'
+import Background1 from './1.jpg'
+import Background2 from './2.png'
+import Background3 from './3.png'
 const Ownershipform = () => {
     return (
         <div  className={OwnershipformCss.fullwrapper}>
             <h1 >HERE IS HOW WE CAN HELP</h1>
             <div className={OwnershipformCss.wrapper}>
                 <div className={OwnershipformCss.insidewrapper}>
-                   <div className={OwnershipformCss.img} >
+                   <div className={OwnershipformCss.img} style={{backgroundImage: `url(${Background1})`}}>
                         <div className={OwnershipformCss.title}>
                             <p>FILL THE FORM</p>
                         </div>
@@ -19,7 +19,7 @@ const Ownershipform = () => {
                     </div>
                 </div> 
                 <div className={OwnershipformCss.insidewrapper}>
-                   <div className={OwnershipformCss.img} >
+                   <div className={OwnershipformCss.img} style={{backgroundImage: `url(${Background2})`}}>
                         <div className={OwnershipformCss.title}>
                             <p>REVIEW</p>
                         </div>
@@ -29,7 +29,7 @@ const Ownershipform = () => {
                     </div>
                 </div> 
                 <div className={OwnershipformCss.insidewrapper}>
-                   <div className={OwnershipformCss.img} >
+                   <div className={OwnershipformCss.img} style={{background: `url(${Background3})`, backgroundSize: '100%', backgroundPosition: '-7px -76px', backgroundRepeat: 'no-repeat'}}>
                         <div className={OwnershipformCss.title}>
                             <p>RESPONSE</p>
                         </div>
