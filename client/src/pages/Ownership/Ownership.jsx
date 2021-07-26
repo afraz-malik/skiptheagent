75e41e1d040afb372736744b3e42909c882a671b
Afraz Malik <afrazmalik321@gmail.com>
2021-07-26 23:09:00 +0500

    'ok'

--- client/src/pages/Ownership/Ownership.jsx
+++ client/src/pages/Ownership/Ownership.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // Components
 import Header from '../../components/Header/Header'
 import OwnershipBoxModel from '../../components/ownershipBoxModel/ownershipBoxModel'
-import RulesBox from '../../components/RulesBox/RulesBoxx'
+import RulesBox from '../../components/RulesBox/RulesBox'
 import Ownershipform from '../../components/Ownershipform/Ownershipform'
 import Footer from '../../components/Footer/Footer'
 
