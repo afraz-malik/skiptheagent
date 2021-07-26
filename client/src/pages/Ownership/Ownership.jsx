46fcc4f3f92b45bd0358b80b28191e865cee141f
Afraz Malik <afrazmalik321@gmail.com>
2021-07-26 23:08:20 +0500

    'ok'

--- client/src/pages/Ownership/Ownership.jsx
+++ client/src/pages/Ownership/Ownership.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import OwnershipBoxModel from '../../components/ownershipBoxModel/ownershipBoxModel'
 import RulesBox from '../../components/RulesBox/RulesBoxx'
-import Ownershipform from '../../components/Ownershipform/Ownershipformx'
+import Ownershipform from '../../components/Ownershipform/Ownershipform'
 import Footer from '../../components/Footer/Footer'
 
 class Details extends React.Component {
